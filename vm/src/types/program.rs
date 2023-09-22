2fc261f5eb94156806493a2828a7808fa320a198
Harsh Bajpai <harshbajpai@Harshs-MacBook-Pro.local>
2023-09-22 15:28:14 +0700

    dev: make `SharedProgramData` public

--- vm/src/types/program.rs
+++ vm/src/types/program.rs
@@ -52,7 +52,7 @@ use arbitrary::{Arbitrary, Unstructured};
 // failures.
 // Fields in `Program` (other than `SharedProgramData` itself) are used by the main logic.
 #[derive(Clone, Default, Debug, PartialEq, Eq)]
-pub(crate) struct SharedProgramData {
+pub struct SharedProgramData {
     pub(crate) data: Vec<MaybeRelocatable>,
     pub(crate) hints_collection: HintsCollection,
     pub(crate) main: Option<usize>,
